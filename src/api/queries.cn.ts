de623e0b139a68a43c60e6cddd897d313318694c
dong <1057762929@qq.com>
2026-04-21 11:35:11 +0800

    feat: add support for 'list' and other commands for leetcode.cn

--- src/api/queries.cn.ts
+++ src/api/queries.cn.ts
@@ -1,14 +1,96 @@
 // GraphQL queries for leetcode.cn (China schema)
 import {
   DAILY_CHALLENGE_QUERY as DAILY_CHALLENGE_QUERY_GLOBAL,
-  PROBLEM_DETAIL_QUERY,
-  PROBLEM_LIST_QUERY,
   RANDOM_PROBLEM_QUERY,
   SUBMISSION_DETAILS_QUERY,
   SUBMISSION_LIST_QUERY,
   USER_STATUS_QUERY,
   type QueryPack,
 } from './queries.global.js';
+
+export const PROBLEM_LIST_QUERY_CN = `
+  query problemsetQuestionList($categorySlug: String, $limit: Int, $skip: Int, $filters: QuestionListFilterInput) {
+    problemsetQuestionList(
+      categorySlug: $categorySlug
+      limit: $limit
+      skip: $skip
+      filters: $filters
+    ) {
+      total
+      questions {
+        frontendQuestionId
+        title
+        titleCn
+        titleSlug
+        difficulty
+        paidOnly
+        acRate
+        status
+        topicTags {
+          name
+          nameTranslated
+          id
+          slug
+        }
+      }
+    }
+  }
+`;
+
+export const PROBLEM_DETAIL_QUERY_CN = `
+  query questionData($titleSlug: String!) {
+    question(titleSlug: $titleSlug) {
+      questionId
+      questionFrontendId
+      boundTopicId
+      title
+      titleSlug
+      content
+      translatedTitle
+      translatedContent
+      difficulty
+      isPaidOnly
+      acRate
+      likes
+      dislikes
+      isLiked
+      similarQuestions
+      exampleTestcases
+      contributors {
+        username
+        profileUrl
+        avatarUrl
+      }
+      status
+      topicTags {
+        name
+        slug
+        translatedName
+      }
+      companyTagStats
+      codeSnippets {
+        lang
+        langSlug
+        code
+      }
+      stats
+      hints
+      solution {
+        id
+        canSeeDetail
+      }
+      sampleTestCase
+      metaData
+      judgerAvailable
+      judgeType
+      mysqlSchemas
+      enableRunCode
+      enableTestMode
+      libraryUrl
+      note
+    }
+  }
+`;
 
 export const DAILY_CHALLENGE_QUERY_CN = `
   query questionOfToday {
@@ -75,8 +157,8 @@ export const SKILL_STATS_QUERY_CN = `
 `;
 
 export const CN_QUERY_PACK: QueryPack = {
-  PROBLEM_LIST_QUERY,
-  PROBLEM_DETAIL_QUERY,
+  PROBLEM_LIST_QUERY: PROBLEM_LIST_QUERY_CN,
+  PROBLEM_DETAIL_QUERY: PROBLEM_DETAIL_QUERY_CN,
   USER_STATUS_QUERY,
   USER_PROFILE_QUERY: USER_PROFILE_QUERY_CN,
   SKILL_STATS_QUERY: SKILL_STATS_QUERY_CN,
